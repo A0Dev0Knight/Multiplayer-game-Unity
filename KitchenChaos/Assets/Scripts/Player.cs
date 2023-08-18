5a239f792a58a600424c81c4b23d06b8efabb519
AlexDaniel-nice <112327676+A0Dev0Knight@users.noreply.github.com>
2023-08-18 14:08:13 +0300

    movement works, diagonal movement is faster
    the magnitude of the vector on any diagonal direction is higher than 1 (sqrt(2) actually, so 1,41)

--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -6,10 +6,30 @@ public class Player : MonoBehaviour
 {
     private void Update()
     {
+        Vector2 inputVector = Vector2.zero;
+
         if (Input.GetKey(KeyCode.W))
         {
-            Debug.Log("FORWARD MY STEED!!");
+            inputVector.y = +1;
         }
-        else Debug.Log("---");
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;
+        }
+
+        //inputVector = inputVector.normalized;
+
+        Debug.Log(inputVector);
     }
 }
