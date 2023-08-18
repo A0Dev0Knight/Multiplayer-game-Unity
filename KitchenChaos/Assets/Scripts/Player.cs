88ffed3c4c7297d4f75e009138ac342e6c68419b
AlexDaniel-nice <112327676+A0Dev0Knight@users.noreply.github.com>
2023-08-18 14:58:45 +0300

    the character now rotates
    interesting why the character keep facing the same direction if moveDir is 0 when no input is given, the game should turn th eplayer to face forward of 0 over time

--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
 
     [SerializeField] private float moveSpeed = 10;
+    [SerializeField] private float rotateSpeed = 10;
     private void Update()
     {
         Vector2 inputVector = Vector2.zero;
@@ -31,10 +32,11 @@ public class Player : MonoBehaviour
         }
 
         inputVector = inputVector.normalized;
+        
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
 
-        Debug.Log(Time.deltaTime);
     }
 }
