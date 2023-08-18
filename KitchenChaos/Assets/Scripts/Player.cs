388d2d9079084d7406fdbdb40fd246463216dc17
AlexDaniel-nice <112327676+A0Dev0Knight@users.noreply.github.com>
2023-08-18 18:54:23 +0300

    animations for walking now change correctly

--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
 
     [SerializeField] private float moveSpeed = 10;
     [SerializeField] private float rotateSpeed = 10;
+
+    private bool isWalking;
     private void Update()
     {
         Vector2 inputVector = Vector2.zero;
@@ -36,7 +38,14 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+        isWalking = moveDir != Vector3.zero;
+
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
 
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
