337cea7bf1179c5c780c4f7fdc4cc1fe869046fd
AlexDaniel-nice <112327676+A0Dev0Knight@users.noreply.github.com>
2023-08-20 18:23:16 +0300

    New input system works
    also added support for arrow keys and controllers

---
+++ KitchenChaos/Assets/Scripts/GameInput.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    private PlayerInputActions playerInputActions;
+    private void Awake()
+    {
+        // PlayerInputActions is the class i have generated in the asset folder
+           playerInputActions = new PlayerInputActions();
+        
+        // by defalut the action maps are desabled so i have to activate the right one
+           playerInputActions.Player.Enable();
+    }
+    public Vector2 GetMovementVectorNormalized()
+    {
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
+        inputVector = inputVector.normalized;
+        Debug.Log(inputVector);
+        return inputVector;
+    }
+}
