ff5a7b7f1dee379b663d22c5e356c1d8c190216b
AlexDaniel-nice <112327676+A0Dev0Knight@users.noreply.github.com>
2023-08-21 15:32:28 +0300

    Player can interact with counter - events implementation

--- KitchenChaos/Assets/Scripts/GameInput.cs
+++ KitchenChaos/Assets/Scripts/GameInput.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GameInput : MonoBehaviour
 {
+    public event EventHandler OnInteractAction;
     private PlayerInputActions playerInputActions;
     private void Awake()
     {
@@ -12,7 +14,15 @@ public class GameInput : MonoBehaviour
         
         // by defalut the action maps are desabled so i have to activate the right one
            playerInputActions.Player.Enable();
+        playerInputActions.Player.Interact.performed += Interact_performed;
     }
+
+    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        OnInteractAction?.Invoke(this, EventArgs.Empty);
+        
+    }
+
     public Vector2 GetMovementVectorNormalized()
     {
         Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Player : MonoBehaviour
 {
 
@@ -12,6 +13,34 @@ public class Player : MonoBehaviour
 
     private bool isWalking;
     private Vector3 lastInteractDir;
+
+    private void Start()
+    {
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e)
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractDir = moveDir;
+        }
+
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                // Has the ClearCounter component
+
+                clearCounter.Interact();
+            }
+        }
+    }
 
     private void Update()
     {
@@ -94,12 +123,8 @@ public class Player : MonoBehaviour
             {
                 // Has the ClearCounter component
 
-                clearCounter.Interact();
+               // clearCounter.Interact();
             }
-        }
-        else
-        {
-            Debug.Log("---");
         }
     }
     public bool IsWalking()
