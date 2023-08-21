35343429c427291651217d5b98805201f46a0ccc
AlexDaniel-nice <112327676+A0Dev0Knight@users.noreply.github.com>
2023-08-21 12:39:27 +0300

    XZ movement speed now fixed

--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -27,7 +27,7 @@ public class Player : MonoBehaviour
             // Cannot move to desired position
 
             // Atempt moving on X axis
-            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
             canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
 
             if(canMove)
@@ -39,7 +39,7 @@ public class Player : MonoBehaviour
             {
                 // Cannot move on X so we try moving on the Z axis
 
-                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
                 canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
 
                 if (canMove)
